878c149fcd9938b2a4772038921c7fc67a6ed658
Suvrajit karmaker <suvrajit.karmaker@bjitgroup.com>
2020-02-26 09:37:46 +0600

    first commit

---
+++ force-app/main/default/aura/BeerExplorer/BeerExplorerController.js
@@ -0,0 +1,27 @@
+({
+	handleCompEvent : function(component, event, helper) {
+		var searchParam = event.getParam('searchText');
+        var action = component.get('c.searchBeer');
+        action.setParams({
+            searchParam:searchParam            
+        });
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if(state ==='SUCCESS')
+            {
+                var responseValue=response.getReturnValue();
+                console.log(' responseValue ',responseValue);
+                component.set('v.beerList',responseValue);
+            }
+            else{
+                console.log(response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    updateCart : function(component, event, helper) {
+        var params= event.getParam('beerRecord');
+        var headerComp=component.find('headerComp');
+        headerComp.updateCart(params);
+    }
+})
